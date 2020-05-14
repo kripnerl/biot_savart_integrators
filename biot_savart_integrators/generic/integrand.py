7067dd3246ee1f0b3bdd032ea9b91ebcfb791588
kripner <kripner@ipp.cas.cz>
2020-05-14 14:13:44 +0200

    enhancement: numerical optimization SI normalization after sumation

--- biot_savart_integrators/generic/integrand.py
+++ biot_savart_integrators/generic/integrand.py
@@ -1,6 +1,6 @@
 """Numerical routines for Biot-Savart integration of a generic current density
 
-The routines expect (dask-chunked) xarray inputs in order to facilitate 
+The routines expect (dask-chunked) xarray inputs in order to facilitate
 memory-efficient and parallelized computation.
 """
 from scipy.constants import mu_0, pi
@@ -12,7 +12,7 @@ _SI_FACTOR = mu_0 / (4*pi)
 
 def biot_savart_integrand(r, r_j, j, spatial_dim):
     R = r - r_j
-    numerator = _SI_FACTOR * cross(j, R, spatial_dim)
+    numerator = cross(j, R, spatial_dim)
     denominator = (R**2).sum(dim=spatial_dim)**(3/2.)
-    integrand = numerator / denominator
+    integrand = _SI_FACTOR * numerator / denominator
     return integrand
